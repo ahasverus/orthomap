d65ffbfd0f48e5011907aa14bc2ef83ae62cbfb8
Nicolas Casajus <nicolas.casajus@gmail.com>
2017-12-06 13:18:49 -0500

    Create orthomap function

---
+++ R/orthomap.R
@@ -0,0 +1,28 @@
+#' @title Create an orthographic World map
+#'
+#' @description
+#' ...
+#'
+#' @param ...
+#'
+#' @export
+#'
+#' @return ...
+#'
+#' @details ...
+#'
+#' @seealso \code{\link[maps]{map}}
+#'
+#' @examples
+#' # ...
+
+orthomap <- function(
+  query    = NULL, 
+  centre   = c(0, 0),
+  rotation = 0,
+  border   = "#FFFFFF",
+  fill     = "#CCCCCC"
+) {
+
+
+}
